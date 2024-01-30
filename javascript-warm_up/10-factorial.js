4e3dcc3145ede30531deccb0c7ee0a71dc2a0c68
cysron-b <b.cysron@alustudent.com>
2024-01-30 05:26:42 -0800

    Mandatory Task

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 function factorial(n) {
 	  if (isNaN(n) || n < 0) return 1;
 	  if (n === 1 || n === 0) return 1;
@@ -8,3 +7,4 @@ function factorial(n) {
 
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
+
