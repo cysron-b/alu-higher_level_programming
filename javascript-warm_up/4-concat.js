d2597abc03f3637de3d9e9cc9b6e0d0fe3334a01
cysron-b <b.cysron@alustudent.com>
2024-01-30 06:33:18 -0800

    a

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 console.log(process.argv[2] + ' is ' + process.argv[3]);
-=======
-#!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
->>>>>>> 3bf4702e31bc60640111855ef70115f98268bc9b
