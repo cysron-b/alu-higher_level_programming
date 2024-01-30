8b39bdfaeaa9cd43b59af858fa5d47b8927c056c
cysron-b <139706271+cysron-b@users.noreply.github.com>
2024-01-30 16:15:18 +0200

    Mandatory Task

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
   console.log('No argument');
+} else {
+  console.log(args[0]);
 }
