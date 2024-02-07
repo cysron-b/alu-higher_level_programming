979f897346ce914941e2eddba04c6fa208603e96
cysron-b <139706271+cysron-b@users.noreply.github.com>
2024-02-07 16:22:03 +0200

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const file = process.argv.slice(2);
 
 fs.writeFile(file[0], file[1], 'utf8', (err) => {
-	  if (err) {
-		      console.log(err);
-		    }
+  if (err) {
+    console.log(err);
+  }
 });
